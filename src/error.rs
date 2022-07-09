1e96cf0ca91a6316e467d840a4e2cb7ca057eaef
Suirabu <suirabu.dev@gmail.com>
2022-07-08 17:10:37 -0700

    Added `SourceError` error variant
    Added new error variant for displaying errors with a source path and
    position. This variant should be used for reporting errors which
    directly relate to a lexemme in Chai source code.

--- src/error.rs
+++ src/error.rs
@@ -4,6 +4,7 @@ use std::fmt;
 #[derive(Debug)]
 pub enum ChaiError {
     StandardError(String),
+    SourceError(String, (usize, usize), String),
 }
 
 impl fmt::Display for ChaiError {
@@ -11,7 +12,15 @@ impl fmt::Display for ChaiError {
         match self {
             ChaiError::StandardError(s) => {
                 f.write_fmt(format_args!("{} {}", "Error:".red().bold(), s))
-            }
+            },
+            ChaiError::SourceError(source, position, s) => {
+                f.write_fmt(format_args!(
+                    "{} {} {}",
+                    "Error:".red().bold(),
+                    format!("{} ({},{}):", source, position.0 + 1, position.1 + 1).bold(),
+                    s
+                ))
+            },
         }
     }
 }
