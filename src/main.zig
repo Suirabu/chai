67f843ae513d26083e04d1da02f28c80cf279160
Suirabu <suirabu.dev@gmail.com>
2022-08-17 06:27:22 -0700

    Avoid using unnecessary allocators in main
    Before we were using three seperate allocators for allocating command
    line arguments, reading from files, and lexing. These three tasks now
    all use the same allocator.

--- src/main.zig
+++ src/main.zig
@@ -6,27 +6,6 @@ const Lexer = @import("lexer.zig").Lexer;
 const log_level: std.log.Level = .debug;
 
 pub fn main() !void {
-    // Collect command line arguments
-    var args_allocator = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    defer args_allocator.deinit();
-    var args = try std.process.argsWithAllocator(args_allocator.allocator());
-    // Because we are only targetting POSIX systems for now we can be sure that
-    // our arguments will contain at least one element, namely the path of
-    // our executable, which we can safely ignore
-    _ = args.skip();
-    const source_path = args.next() orelse {
-        std.log.err("Expected source path", .{});
-        return error.MissingSourcePath;
-    };
-
-    var file_allocator = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    defer file_allocator.deinit();
-    const source_file = fs.cwd().openFile(source_path, .{}) catch |err| {
-        std.log.err("Failed to open file '{s}' for reading", .{source_path});
-        return err;
-    };
-    const source = try source_file.readToEndAlloc(file_allocator.allocator(), 1 << 32);
-
     // Initialize allocators
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer {
@@ -38,6 +17,23 @@ pub fn main() !void {
 
     var arena = std.heap.ArenaAllocator.init(gpa.allocator());
     defer arena.deinit();
+
+    // Collect command line arguments
+    var args = try std.process.argsWithAllocator(arena.allocator());
+    // Because we are only targetting POSIX systems for now we can be sure that
+    // our arguments will contain at least one element, namely the path of
+    // our executable, which we can safely ignore
+    _ = args.skip();
+    const source_path = args.next() orelse {
+        std.log.err("Expected source path", .{});
+        return error.MissingSourcePath;
+    };
+
+    const source_file = fs.cwd().openFile(source_path, .{}) catch |err| {
+        std.log.err("Failed to open file '{s}' for reading", .{source_path});
+        return err;
+    };
+    const source = try source_file.readToEndAlloc(arena.allocator(), 1 << 32);
 
     var lexer = Lexer.initFromSource(source, source_path, arena.allocator());
     const tokens = try lexer.collectTokens();
