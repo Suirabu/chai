e109e73c94ce01879f52de1890f25d74b32c55ac
Suirabu <suirabu.dev@gmail.com>
2022-09-01 21:42:34 -0700

    Automatically generate executable from generated assembly

--- src/main.zig
+++ src/main.zig
@@ -32,10 +32,16 @@ pub fn main() !void {
         return error.MissingSourcePath;
     };
 
+    if (!std.mem.endsWith(u8, source_path, ".chai")) {
+        std.log.err("Source file must use '.chai' file extension", .{});
+        return error.InvalidFileExtension;
+    }
+
     const source_file = fs.cwd().openFile(source_path, .{}) catch |err| {
         std.log.err("Failed to open file '{s}' for reading", .{source_path});
         return err;
     };
+    defer source_file.close();
     const source = try source_file.readToEndAlloc(arena.allocator(), 1 << 32);
 
     var lexer = Lexer.initFromSource(source, source_path, arena.allocator());
@@ -47,7 +53,22 @@ pub fn main() !void {
     var type_checker = TypeChecker.init(exprs, arena.allocator());
     try type_checker.check();
 
-    var stdout = std.io.getStdOut();
-    var code_generator = CodeGenerator.init(exprs, stdout);
-    try code_generator.generate_x86_64_intel_linux();
+    // Code generation
+    {
+        var out_file = try fs.cwd().createFile(".chai_out.asm", .{});
+        defer out_file.close();
+
+        var code_generator = CodeGenerator.init(exprs, out_file);
+        try code_generator.generate_x86_64_intel_linux();
+    }
+
+    defer {
+        fs.cwd().deleteFile(".chai_out.asm") catch unreachable;
+    }
+    _ = try std.ChildProcess.exec(.{ .allocator = arena.allocator(), .argv = &.{ "yasm", "-f", "elf64", ".chai_out.asm", "-o", ".chai_out.o" } });
+    defer {
+        fs.cwd().deleteFile(".chai_out.o") catch unreachable;
+    }
+    const executable_name = source_path[0 .. source_path.len - 5];
+    _ = try std.ChildProcess.exec(.{ .allocator = arena.allocator(), .argv = &.{ "ld", ".chai_out.o", "-o", executable_name } });
 }
